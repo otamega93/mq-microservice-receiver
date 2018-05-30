d29bf79751e1c63e951e3014ad41df72d9917148
Manuel Páez <m.paez589@gmail.com>
2018-05-30 11:04:56 +0200

    Add files via upload

---
+++ src/main/java/com/example/mqmicroservicereceiver/MqMicroserviceReceiverApplication.java
@@ -0,0 +1,12 @@
+package com.example.mqmicroservicereceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqMicroserviceReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqMicroserviceReceiverApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/mqmicroservicereceiver/MqMicroserviceReceiverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.mqmicroservicereceiver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MqMicroserviceReceiverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
